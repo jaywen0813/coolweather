380d31b0d77c73132af856687575757e7f5e23cc
wenjie <330929643@qq.com>
2017-11-30 17:46:24 +0800

    修改了键值对

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -137,7 +137,7 @@ public class ChooseAreaFragment extends Fragment {
         }else {
             int provinceCode=selectedProvince.getProvinceCode();
             String address="http://guolin.tech/api/china/"+provinceCode;
-            queryFromServer(address,"City");
+            queryFromServer(address,"city");
         }
     }
 
