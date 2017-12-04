32d0cefe708507ddd88c837512a224c87831cfed
wenjie <330929643@qq.com>
2017-12-04 09:56:58 +0800

    完整版天气预报

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,14 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by wenjie on 2017/11/30 0030.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+
+}
